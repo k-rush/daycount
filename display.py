bdfc20144d43be429456ea196fbde17cdc768645
Kyle Rush <krush@sonobello.com>
2018-09-13 22:45:09 -0700

    Daycount test

---
+++ display.py
@@ -0,0 +1,31 @@
+import _mysql
+import time
+import datetime
+import math
+import pygame
+from Adafruit_LED_Backpack import SevenSegment
+
+#initialize display
+
+display = SevenSegment.SevenSegment(address=0x70)
+display.begin()
+
+#debug
+print datetime.datetime.now()
+
+sober = date(2016, 8, 18)
+daycount = datetime.datetime.now() - sober
+
+print daycount
+
+#-----------display-------------------
+
+#totalDisplay.clear()
+
+unclosedDisplay.clear()
+
+unclosedDisplay.set_digit(3, int(daycount) % 10) #ones
+unclosedDisplay.set_digit(2, int(int(daycount)/10) % 10) #tens
+unclosedDisplay.set_digit(1, int(int(daycount)/100) % 10) #hundreds
+unclosedDisplay.set_digit(0, int(int(daycount)/1000) % 10) #thousands
+unclosedDisplay.write_display()
